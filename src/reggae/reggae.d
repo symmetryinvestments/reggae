22702344fe24c9bf777e1ee816c2447a913e41ac
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2024-01-30 18:12:09 +0100

    Remove superfluous unit-threaded selection for buildgen

--- src/reggae/reggae.d
+++ src/reggae/reggae.d
@@ -269,8 +269,7 @@ private enum reggaeFileDubSelectionsJson =
         "fileVersion": 1,
         "versions": {
                 "dub": %s,
-                "reggae": {"path":"packages/reggae"},
-                "unit-threaded": %s
+                "reggae": {"path":"packages/reggae"}
         }
 }
 `;
@@ -322,7 +321,7 @@ private Binary buildReggaefileDub(O)(auto ref O output, in Options options) {
     writeIfDiffers(
         output,
         buildPath(dubRecipeDir, "dub.selections.json"),
-        reggaeFileDubSelectionsJson.format(selectionsPkgVersion!"dub", selectionsPkgVersion!"unit-threaded"),
+        reggaeFileDubSelectionsJson.format(selectionsPkgVersion!"dub"),
     );
 
     const reggaeRecipePath = buildPath(reggaeSrcDirName(options), "..", "dub.sdl");
